5a347b2efda8b00e697ce6584498273229bf7a59
Nils006 <38532869+Nils006@users.noreply.github.com>
2018-05-13 22:12:57 +0200

    Kugeln zählen

--- Abakus_Testataufgabe_SoSe2018/js/script.js
+++ Abakus_Testataufgabe_SoSe2018/js/script.js
@@ -2,80 +2,28 @@
  * http://usejsdoc.org/
  */
 
-
-$( document ).ready(function() {
-
-    $( "#test" ).click(function( event ) {
-
-        alert( $("#stangen").children().length );
-
-    });
-
-});
-
-
 $( document ).ready(function() {
 
     $( ".kugel" ).click(function( event ) {
 
+      //bewegt Kugeln beim KLicken nach Links
     	if ($(this).hasClass("rechts")) {
         $(this).removeClass("rechts").addClass("links");
         $(this).prevAll().removeClass("rechts").addClass("links");
-    		// nach links..
     	}
-      else {
-        if ($(this).hasClass("links")) {
-          $(this).removeClass("links")
-        }
-        $(this).addClass("rechts");
-        $(this).nextAll().addClass("rechts");
+      //bewegt Kugeln beim KLicken nach Rechts
+      else if ($(this).hasClass("links")) {
+        $(this).removeClass("links").addClass("rechts");
+        $(this).nextAll().removeClass("links").addClass("rechts");
       }
 
+        var ausgabe = 0;
+        var ausgabe = $("#1stelle").children(".rechts").length + $("#2stelle").children(".rechts").length*10 + $("#3stelle").children(".rechts").length * 100 + $("#4stelle").children(".rechts").length *1000 + $("#5stelle").children(".rechts").length *10000;
 
+        $("#anzeige").text(ausgabe);
+        $("#ausgabe").text(ausgabe);
 
-        // rechne(); -> Für die Anzeige oben rechts und unten links
 
     });
 
 });
-
-
-
- /*function count(){
-  *
-  *
-
-$( document ).ready(function() {
-
-    $( ".kugel" ).click(function( event ) {
-
-        $(this).parent().css('text-align','right');
-
-    });
-
-});
-
-
-
-
-});
-
-   var stangen = $("#stangen").children().length;
-   var ausgabe;
-
-   for (var i = 0; i < stangen; i++) {
-     var j =
-   }
-   var element = document.getElementById("1stelle");
-   var numberOfChildren = element.children.length
-
-
-  alert(numberOfChildren);
- }
-
- class Row  {
-
-   var left = 10;
-   var right = 0;
-
- } */
