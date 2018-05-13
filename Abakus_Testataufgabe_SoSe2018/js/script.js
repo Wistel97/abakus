e36a9d113452e17739dcea1028b616ac75e4f8b5
Nils006 <38532869+Nils006@users.noreply.github.com>
2018-05-13 16:42:43 +0200

    jquery

--- Abakus_Testataufgabe_SoSe2018/js/script.js
+++ Abakus_Testataufgabe_SoSe2018/js/script.js
@@ -1,3 +1,47 @@
 /**
  * http://usejsdoc.org/
  */
+
+
+$( document ).ready(function() {
+
+    $( "#test" ).click(function( event ) {
+
+        alert( $("#stangen").children().length );
+
+    });
+
+});
+
+$( document ).ready(function() {
+
+    $( ".kugel" ).click(function( event ) {
+
+        $(this).parent().css('text-align','right');
+
+    });
+
+});
+
+ /*function count(){
+
+
+   var stangen = $("#stangen").children().length;
+   var ausgabe;
+
+   for (var i = 0; i < stangen; i++) {
+     var j =
+   }
+   var element = document.getElementById("1stelle");
+   var numberOfChildren = element.children.length
+
+
+  alert(numberOfChildren);
+ }
+
+ class Row  {
+
+   var left = 10;
+   var right = 0;
+
+ } */
